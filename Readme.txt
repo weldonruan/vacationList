c2a1fb15db3128395bfd4babc60e0bf5bd3b86a4
Weldon Ruan <weldonruan@WeldondeMacBook-Air.local>
2015-02-14 23:16:09 +0800

    2

---
+++ Readme.txt
@@ -0,0 +1,13 @@
+xxxxxxxxxx
+
+增加Navation Controller控制条的方法
+选中Main.storyboard的controller，然后在菜单Editor - Embed in 选中 Navigation controller
+
+Navation Controller中增加按钮的控件
+Tab Bar Item
+
+自行接管storyboard中Controller的方法
+1.新建一个File,定义一个类名,继承自ViewController
+2.在storyboard中选中Controller,在最右边的identity inspector下，选择刚才新建的类名
+
+课时13已经看完
---
+++ vacationList/AddVacationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddVacationViewController.swift
+//  vacationList
+//
+//  Created by Weldon Ruan on 15/2/14.
+//  Copyright (c) 2015年 Weldon Ruan. All rights reserved.
+//
+
+import UIKit
+
+class AddVacationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
