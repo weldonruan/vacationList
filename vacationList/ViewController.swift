0fce2765d03b92f91ebd9aae2393453476908fc1
Weldon Ruan <weldonruan@WeldondeMacBook-Air.local>
2015-02-14 21:24:35 +0800

    Initial Commit

---
+++ vacationList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  vacationList
+//
+//  Created by Weldon Ruan on 15/2/14.
+//  Copyright (c) 2015年 Weldon Ruan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
